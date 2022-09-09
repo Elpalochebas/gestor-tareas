969c03cf7f3f9558b2f83cb77d162bf9da968d8b
Elpalochebas <elpalochebas@gmail.com>
2022-09-08 21:46:32 -0500

    mejoras

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+from datetime import datetime
 import db
 
 from models import Tarea
@@ -9,12 +10,18 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     tareas_db = db.session.query(Tarea).all()
-    return render_template('index.html', lista_tareas=tareas_db)
+    
+    categorias = db.session.execute('SELECT DISTINCT categoria FROM tarea')
+
+    return render_template('index.html', lista_tareas=tareas_db, lista_categorias=categorias)
 
 
 @app.route('/crear-tarea', methods=['POST'])
 def crear():
-    tarea = Tarea(contenido=request.form['contenido_tarea'], hecha=False)
+    tarea = Tarea(  contenido=request.form['contenido_tarea'],
+                    hecha=False,
+                    categoria=request.form['categoria_tarea'],
+                    fecha=datetime.now())
     db.session.add(tarea)
     db.session.commit()
     return redirect(url_for('home'))
--- models.py
+++ models.py
@@ -1,15 +1,19 @@
 import db
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
 
 class Tarea(db.Base):
     __tablename__='tarea'
     id = Column(Integer, primary_key=True)
     contenido = Column(String(200), nullable=False)
     hecha = Column(Boolean)
+    categoria = Column(String(200), nullable=False)
+    fecha = Column(DateTime)
 
-    def __init__(self,contenido, hecha):
+    def __init__(self,contenido, hecha, categoria, fecha):
         self.contenido = contenido
         self.hecha = hecha
+        self.categoria = categoria
+        self.fecha = fecha
     
     def __repr__(self):
         return f"Tarea {self.id}: {self.contenido} ({self.hecha})"
