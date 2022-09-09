fa2445ba99f0b0577a042c97b20d64240dbb756b
Elpalochebas <elpalochebas@gmail.com>
2022-09-09 14:09:06 -0500

    mejoras 1

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     tareas_db = db.session.query(Tarea).all()
-    
+
+    for tarea in tareas_db:
+        tarea.date_format()
+
     categorias = db.session.execute('SELECT DISTINCT categoria FROM tarea')
 
     return render_template('index.html', lista_tareas=tareas_db, lista_categorias=categorias)
@@ -21,16 +24,39 @@ def crear():
     tarea = Tarea(  contenido=request.form['contenido_tarea'],
                     hecha=False,
                     categoria=request.form['categoria_tarea'],
-                    fecha=datetime.now())
-    db.session.add(tarea)
-    db.session.commit()
+                    fecha_creacion=datetime.now(),
+                    fecha_entrega=datetime.strptime(request.form['fecha_tarea'], '%Y-%m-%dT%H:%M'))
+
+    try: 
+        db.session.add(tarea)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(e)
+    return redirect(url_for('home'))
+
+@app.route('/update-tarea/<id>', methods=['POST'])
+def actualizar(id):
+    db.session.query(Tarea).filter_by(id=int(id)).update(dict(
+        contenido=request.form['contenido_tarea'], 
+        categoria=request.form['categoria_tarea'],
+        fecha_entrega=datetime.strptime(request.form['fecha_tarea'], '%Y-%m-%dT%H:%M')))
+    try: 
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(e)
     return redirect(url_for('home'))
 
 
 @app.route('/eliminar-tarea/<id>')
 def eliminar(id):
-    tarea = db.session.query(Tarea).filter_by(id=int(id)).delete()
-    db.session.commit()
+    db.session.query(Tarea).filter_by(id=int(id)).delete()
+    try: 
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        print(e)
     return redirect(url_for('home'))
 
 
@@ -38,7 +64,10 @@ def eliminar(id):
 def hecha(id):
     tarea = db.session.query(Tarea).filter_by(id=int(id)).first()
     tarea.hecha = not (tarea.hecha)
-    db.session.commit()
+    try:
+        db.session.commit()
+    except Exception as e:
+        print(e)
     return redirect(url_for('home'))
 
 
--- models.py
+++ models.py
@@ -1,19 +1,29 @@
 import db
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from datetime import datetime
 
 class Tarea(db.Base):
     __tablename__='tarea'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     contenido = Column(String(200), nullable=False)
     hecha = Column(Boolean)
     categoria = Column(String(200), nullable=False)
-    fecha = Column(DateTime)
+    fecha_creacion = Column(DateTime)
+    fecha_entrega = Column(DateTime)
+    fecha_creacion_str:str
+    fecha_entrega_str:str
 
-    def __init__(self,contenido, hecha, categoria, fecha):
+    def __init__(self,contenido, hecha, categoria, fecha_creacion, fecha_entrega):
         self.contenido = contenido
         self.hecha = hecha
-        self.categoria = categoria
-        self.fecha = fecha
+        self.categoria = categoria 
+        self.fecha_creacion = fecha_creacion
+        self.fecha_entrega = fecha_entrega
+    
+    def date_format (self):
+        self.fecha_creacion_str = datetime.strftime(self.fecha_creacion, '%Y-%m-%d %H:%M')
+        self.fecha_entrega_str = datetime.strftime(self.fecha_entrega, '%Y-%m-%d %H:%M')
+       
     
     def __repr__(self):
         return f"Tarea {self.id}: {self.contenido} ({self.hecha})"
